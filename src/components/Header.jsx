74840ba514136ecea2f62d0f2d586eaadb8b4f47
lianpaulm <lianpaulmolo08@gmail.com>
2023-01-19 01:13:34 +0800

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import Logo from "../assets/images/logo.svg";
+import navLinks from "../data";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // remove sidebar when width of window is greater then 768px
+  const onResize = (e) => {
+    if (e.currentTarget.innerWidth > 768) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
+
+  // remove scroll when sidebar open
+  isMenuOpen
+    ? (document.body.style.overflow = "hidden")
+    : (document.body.style.overflow = "visible");
+
+  return (
+    <header className="relative bg-white h-[60px] md:h-[80px] ">
+      {/* container */}
+      <div className="flex items-center justify-between h-full max-w-6xl mx-auto px-4 md:px-6 ">
+        {/* logo */}
+        <a href="#" className="shrink-0 max-w-[128px] md:max-w-none">
+          <img src={Logo} alt="logo" />
+        </a>
+
+        {/* hamburger menu */}
+        <button
+          id="menu-btn"
+          className={`${isMenuOpen && "open"} hamburger block md:hidden`}
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+        >
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
+
+        {/* overlay bg */}
+        <div
+          className={`${
+            isMenuOpen ? "block" : "hidden"
+          } md:hidden fixed inset-x-0 top-[60px] bottom-0 bg-gradient-to-b from-dark-blue to-transparent opacity-90`}
+          aria-hidden="true"
+        ></div>
+
+        {/* navbar */}
+        <nav
+          className={`md:static md:flex md:flex-row md:gap-8 md:p-0 md:rounded-none ${
+            isMenuOpen ? "flex" : "hidden"
+          } flex-col gap-4 items-center p-8 bg-white absolute top-[84px] left-[24px] right-[24px] rounded-md`}
+        >
+          {navLinks.map((link, index) => {
+            return (
+              <a
+                href={link.url}
+                key={index}
+                className="nav-link text-lg md:text-grayish-blue md:hover:text-dark-blue text-dark-blue transition-colors hover:text-grayish-blue"
+              >
+                {link.name}
+              </a>
+            );
+          })}
+        </nav>
+
+        {/* nav cta */}
+        <button className="hidden md:block text-white py-3 px-8 rounded-full bg-gradient-to-r from-lime-green to-bright-cyan hover:opacity-75 transition-opacity">
+          Request Invite
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const navLinks = [
+  {
+    name: 'Home',
+    url: '#'
+  },
+  {
+    name: 'About',
+    url: '#'
+  },
+  {
+    name: 'Content',
+    url: '#'
+  },
+  {
+    name: 'Blog',
+    url: '#'
+  },
+  {
+    name: 'Careers',
+    url: '#'
+  }
+]
+
+export default navLinks
